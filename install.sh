d892c85bed8faab6471ebde5dfd6fc0d5eba7ce9
Emre <emrekizilelma@users.noreply.github.com>
2025-08-05 20:06:50 +0300

    update: install.sh

--- install.sh
+++ install.sh
@@ -2,47 +2,47 @@
 
 show_menu()
 {
-  echo "*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*"
-  echo "-- Fedora Post Installation Script -- "
-  echo "Choose the Software that want to install"
-  echo "1.) Brave Browser"
-  echo "2.) ZSH"
-  echo "3.) NvimChad"
-  echo "4.) Rpmfusion"
-  echo "5.) All"
-  echo "*-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+    echo "*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*"
+    echo "-- Fedora Post Installation Script -- "
+    echo "Choose the Software that want to install"
+    echo "1.) Brave Browser"
+    echo "2.) ZSH"
+    echo "3.) NvimChad"
+    echo "4.) Rpmfusion"
+    echo "5.) All"
+    echo "*-*-*-*-*-**-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
 }
 
 install_brave()
 {
-  curl -fsS https://dl.brave.com/install.sh | sh
+    curl -fsS https://dl.brave.com/install.sh | sh
 }
 
 install_zsh()
 {
-  sudo dnf install zsh -y
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sudo dnf install zsh -y
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
 install_nvimChad()
 {
-  sudo dnf install neovim -y
-  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim  
+    sudo dnf install neovim -y
+    git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim  
 }
 
 install_rpmfusion()
 {
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 install_all()
 {
-  echo "Installing..."
+    echo "Installing..."
     install_brave
     install_zsh
     install_nvimChad
     install_rpmfusion
-  echo "Installed All Programs!"
+    echo "Installed All Programs!"
 }
 
 show_menu
